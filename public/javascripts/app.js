c03d12f79577fcc7b3c83648d0148149e26081c4
AderitoOliveira <aderito.nelson1@gmail.com>
2019-04-23 15:34:53 +0100

    Correclty implemented the change to the top of the page when changing to another page

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -182,7 +182,7 @@ app.config(function ($locationProvider, $stateProvider, $urlRouterProvider) {
 //app.run(['$rootScope', '$location', function($rootScope, $location) {
 //  $rootScope.$location = $location;
 //}]);
-app.run(['$rootScope', '$location', '$cookies', '$http', function ($rootScope, $location, $cookies, $http) {
+app.run(['$rootScope', '$location', '$cookies', '$http', '$anchorScroll', function ($rootScope, $location, $cookies, $http, $anchorScroll) {
   // keep user logged in after page refresh
   $rootScope.globals = $cookies.get('globals') || {};
   if ($rootScope.globals.currentUser) {
@@ -194,6 +194,8 @@ app.run(['$rootScope', '$location', '$cookies', '$http', function ($rootScope, $
     if ($location.path() !== '/login' && !$rootScope.globals.currentUser) {
       $location.path('/login');
     }
+    //Move the scroll back to the top when going to another page
+    $anchorScroll();
   });
 }]);
 
