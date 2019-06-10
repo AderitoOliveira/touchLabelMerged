d4e505af4a3a4b3114e0dca838a9f21ebe0af815
Adérito Oliveira <aderito.nelson1@gmail.com>
2019-06-10 19:06:09 +0100

    Chanegs for the Employee Statistics

--- public/javascripts/appStatistics.js
+++ public/javascripts/appStatistics.js
@@ -38,10 +38,10 @@ statistics.controller('productionRegistryStatisticsController', function ($scope
     opened: false
   };
 
-  $scope.$watch('beginDate', function(){
+  $scope.$watch('beginDate', function () {
     console.log(moment($scope.beginDate).format('YYYY-MM-DD'));
   });
-  $scope.$watch('endDate', function(){
+  $scope.$watch('endDate', function () {
     console.log(moment($scope.endDate).format('YYYY-MM-DD'));
   });
   console.log("BEGIN DATE: " + $scope.beginDate);
@@ -50,15 +50,15 @@ statistics.controller('productionRegistryStatisticsController', function ($scope
   $scope.options = { legend: { display: true } };
 
   executeQueryBetweenDateService.executeQuery($scope.beginDate, $scope.endDate, null).then(function (productionArray) {
-    $scope.productionDays   = productionArray.productionDays;
-    $scope.dataProduction3  = productionArray.dataProduction3;
+    $scope.productionDays = productionArray.productionDays;
+    $scope.dataProduction3 = productionArray.dataProduction3;
   });
 
-  $scope.redrawSearch = function() {
+  $scope.redrawSearch = function () {
 
     executeQueryBetweenDateService.executeQuery($scope.beginDate, $scope.endDate, null).then(function (productionArray) {
-      $scope.productionDays   = productionArray.productionDays;
-      $scope.dataProduction3  = productionArray.dataProduction3;
+      $scope.productionDays = productionArray.productionDays;
+      $scope.dataProduction3 = productionArray.dataProduction3;
     });
 
   }
@@ -78,8 +78,8 @@ statistics.controller('employeeRegistryStatisticsController', function ($scope, 
   $scope.options = { legend: { display: true, position: 'bottom' } };
   $scope.colours = ['#803690', '#00ADF9', '#DCDCDC', '#46BFBD', '#FDB45C', '#949FB1', '#4D5360'];
 
-  $scope.totalProductsProduced  = 0;
-  $scope.totalValueProduced  = 0;
+  $scope.totalProductsProduced = 0;
+  $scope.totalValueProduced = 0;
 
   $scope.today = function () {
     $scope.beginDate = new Date('2019-05-01');
@@ -116,7 +116,7 @@ statistics.controller('employeeRegistryStatisticsController', function ($scope, 
   },
     function errorCallback(data) {
       console.log('Error: ' + data);
-  });
+    });
 
   /* $scope.$watch('nameemployee', function(){
     console.log($scope.nameemployee.EMPLOYEE_NAME);
@@ -132,44 +132,133 @@ statistics.controller('employeeRegistryStatisticsController', function ($scope, 
   console.log("END DATE: " + $scope.endDate);
 
   executeQueryBetweenDateService.executeQuery($scope.beginDate, $scope.endDate, null).then(function (productionArray) {
-    $scope.productionDays   = productionArray.productionDays;
-    $scope.dataProduction3  = productionArray.dataProduction3;
+    $scope.productionDays = productionArray.productionDays;
+    $scope.dataProduction3 = productionArray.dataProduction3;
   });
 
-  $scope.redrawSearch = function(beginDate, endDate, nameemployee) {
+  $scope.redrawSearch = function (beginDate, endDate, nameemployee) {
 
     executeQueryBetweenDateService.executeQuery(beginDate, endDate, nameemployee.EMPLOYEE_NAME).then(function (productionArray) {
-      $scope.productionDays   = productionArray.productionDays;
-      $scope.dataProduction3  = productionArray.dataProduction3;
+      $scope.productionDays = productionArray.productionDays;
+      $scope.dataProduction3 = productionArray.dataProduction3;
 
-      $scope.totalProductsProduced  = productionArray.total_products;
-      $scope.totalValueProduced  = productionArray.value_produced;
+      $scope.totalProductsProduced = productionArray.total_products;
+      $scope.totalValueProduced = productionArray.value_produced;
 
     });
 
-    $scope.exportToPDF();
-
   }
 
-  $scope.exportToPDF = function (){
+  function replaceAll(str, map) {
+    for (key in map) {
+      str2 = str.replace(key, map[key]);
+      str = str2;
+      str2 = null;
+    }
+    return str;
+  }
+
+  var map = {
+    '_EMPLOYEE_NAME_': nameemployee.EMPLOYEE_NAME,
+    '_BEGIN_DATE_': moment($scope.beginDate).format('YYYY-MM-DD'),
+    '_END_DATE_': moment($scope.endDate).format('YYYY-MM-DD'),
+    '_TOTAL_PRODUZIDO_': $scope.totalProductsProduced,
+    '_VALOR_PRODUZIDO_': $scope.totalValueProduced
+  };
+
+  $scope.exportToPDF = function (nameemployee) {
+
+    console.log($scope.totalProductsProduced);
+    console.log($scope.totalValueProduced);
+    console.log(nameemployee.EMPLOYEE_NAME);
+
+    map._EMPLOYEE_NAME_ = nameemployee.EMPLOYEE_NAME;
+    map._TOTAL_PRODUZIDO_ = $scope.totalProductsProduced;
+    map._VALOR_PRODUZIDO_ = $scope.totalValueProduced;
 
     var canvasToImg = document.getElementById("bar").toDataURL();
+
     var chartToPrint = {
       content: [
+           {
+            text: [
+                      { text: '_EMPLOYEE_NAME_', style: 'emploeename' }
+                  ]
+          },
           {
+           columns: [
+       
+          {
+             
+            text: [
+                      { text: 'Data de Inicio: _BEGIN_DATE_', style: 'datebegin' },
+                      { text: '\nData de Fim:  _END_DATE_', style: 'dateend' }
+                  ],
+              style: 'datebegin',
+              
+          },
+          {
+             
+            text: [
+                      { text: 'Total Produtos: _TOTAL_PRODUZIDO_', style: 'dateend' },
+                       { text: '\nValor Produzido: _VALOR_PRODUZIDO_', style: 'dateend' }
+                  ],
+              style: 'dateend',
+              
+          }
+       
+          ]},
+         {
+          columns: [
+                 {
+                
+          alignment: 'left',
               columns: [
                 {
                   image: canvasToImg,
                   width: 540,
                   height: 400
+    
                 }
              ]
           }
-      ] 
+                 
+                ]
+              },
+              {}
+      ],
+      styles: {
+              emploeename: {
+                fontSize: 14,
+                color: 'black',
+                bold: true,
+                alignment: 'center'
+            },
+            datebegin: {
+                fontSize: 14,
+                color: 'black',
+                bold: true,
+                alignment: 'left'
+            },
+            dateend: {
+                fontSize: 14,
+                color: 'black',
+                bold: true,
+                margin: [80, 0, 0, 0],
+                align: 'right',
+            }
+      }
       
-    }
+    } 
 
-    pdfMake.createPdf(chartToPrint).download("C:/XPTO.pdf");
+    var chartPDFTemplateToString = JSON.stringify(chartToPrint);
+    var chartPDFTemplateToStringReplaced = replaceAll(chartPDFTemplateToString, map);
+
+    var chartPDFTemplateToJSON = JSON.parse(chartPDFTemplateToStringReplaced);
+
+    var filename = nameemployee.EMPLOYEE_NAME + "_" + map._BEGIN_DATE_ + "_" + map._END_DATE_ + ".pdf";
+
+    pdfMake.createPdf(chartPDFTemplateToJSON).download(filename);
 
   }
 
@@ -179,68 +268,68 @@ statistics.factory('executeQueryBetweenDateService', ['$http', '$q', function ($
 
   function executeQuery(beginDate, endDate, employyename) {
     var deferred = $q.defer();
-	
-	var productionDays = [];
-	var dataProduction = [];
-	var dataProduction2 = [];
-  var dataProduction3 = [];
-  
-  var totalProductsProduced = 0;
-  var valueProducedInEUR    = 0;
 
-  if(employyename != null) {
-    var request = $http({
-      method: 'GET',
-      url: 'getProductionForEmployeeBetweenBeingEndDate',
-      params: {
+    var productionDays = [];
+    var dataProduction = [];
+    var dataProduction2 = [];
+    var dataProduction3 = [];
+
+    var totalProductsProduced = 0;
+    var valueProducedInEUR = 0;
+
+    if (employyename != null) {
+      var request = $http({
+        method: 'GET',
+        url: 'getProductionForEmployeeBetweenBeingEndDate',
+        params: {
           BEGIN_DATE: moment(beginDate).format('YYYY-MM-DD'),
           END_DATE: moment(endDate).format('YYYY-MM-DD'),
           EMPLOYEE_NAME: employyename
-              }
+        }
       });
 
-  } else {
-    var request = $http({
-      method: 'GET',
-      url: 'getProductionBetweenBeingEndDate',
-      params: {
+    } else {
+      var request = $http({
+        method: 'GET',
+        url: 'getProductionBetweenBeingEndDate',
+        params: {
           BEGIN_DATE: moment(beginDate).format('YYYY-MM-DD'),
           END_DATE: moment(endDate).format('YYYY-MM-DD')
-              }
+        }
       });
-  }
-    
-
-	request.then(function successCallback(response) {
-    production = response.data;
-
-    for (i = 0; i < production.length; i++) {
-
-      productionDays.push(moment(production[i].PRODUCTION_DAY).format('YYYY-MM-DD'));
-      dataProduction.push(production[i].TOTAL_DAY_PRODUCTION);
-      dataProduction2.push(production[i].TOTAL_DAY_VALUE_IN_EUR);
-
-      totalProductsProduced = totalProductsProduced + production[i].TOTAL_DAY_PRODUCTION;
-      valueProducedInEUR    = valueProducedInEUR + production[i].TOTAL_DAY_VALUE_IN_EUR;
-
     }
 
-    dataProduction3.push(dataProduction);
-    dataProduction3.push(dataProduction2);
-	
-	var dataProductionArray = {
-		productionDays: productionDays,
-    dataProduction3: dataProduction3,
-    total_products : totalProductsProduced,
-    value_produced: valueProducedInEUR
-	};
-	
-	deferred.resolve(dataProductionArray);
 
-  },
-    function errorCallback(data) {
-      console.log('Error: ' + data);
-    });
+    request.then(function successCallback(response) {
+      production = response.data;
+
+      for (i = 0; i < production.length; i++) {
+
+        productionDays.push(moment(production[i].PRODUCTION_DAY).format('YYYY-MM-DD'));
+        dataProduction.push(production[i].TOTAL_DAY_PRODUCTION);
+        dataProduction2.push(production[i].TOTAL_DAY_VALUE_IN_EUR);
+
+        totalProductsProduced = totalProductsProduced + production[i].TOTAL_DAY_PRODUCTION;
+        valueProducedInEUR = valueProducedInEUR + production[i].TOTAL_DAY_VALUE_IN_EUR;
+
+      }
+
+      dataProduction3.push(dataProduction);
+      dataProduction3.push(dataProduction2);
+
+      var dataProductionArray = {
+        productionDays: productionDays,
+        dataProduction3: dataProduction3,
+        total_products: totalProductsProduced,
+        value_produced: valueProducedInEUR
+      };
+
+      deferred.resolve(dataProductionArray);
+
+    },
+      function errorCallback(data) {
+        console.log('Error: ' + data);
+      });
 
     return deferred.promise;
   }
